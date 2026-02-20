b1743d586ae19423e085c7d7755fd0e5432eddec
Akhilesh Nishad <144948278+codingexport@users.noreply.github.com>
2026-02-20 18:46:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio-main/', // yaha aapke GitHub repo ka exact name
+   // yaha aapke GitHub repo ka exact name
 });
